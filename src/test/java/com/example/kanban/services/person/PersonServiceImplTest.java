cbdecc9f0c2ada2c3d15a0448bffb26262a88908
Marcin Kilimo <mklimowicz96@wp.pl>
2020-04-22 07:16:46 +0200

    Add junit/mockito test for PersonService.

--- src/test/java/com/example/kanban/services/person/PersonServiceImplTest.java
+++ src/test/java/com/example/kanban/services/person/PersonServiceImplTest.java
@@ -1,11 +1,15 @@
 package com.example.kanban.services.person;
 
 import com.example.kanban.dao.person.PersonDao;
+import com.example.kanban.dto.NoteDto;
 import com.example.kanban.dto.PersonDto;
+import com.example.kanban.dto.ProjectDto;
 import com.example.kanban.exception.person.NotFoundPersonException;
 import com.example.kanban.mapper.PersonMapper;
 import com.example.kanban.mapper.ProjectMapper;
+import com.example.kanban.model.Note;
 import com.example.kanban.model.Person;
+import com.example.kanban.model.Project;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -46,6 +50,8 @@ class PersonServiceImplTest {
         projectMapper = new ProjectMapper();
         person = getPersonList().get(0);
         personDto = personMapper.toDto(person);
+        person.setNotes(getListNote());
+        person.setProjects(getListProject());
 
     }
 
@@ -93,7 +99,24 @@ class PersonServiceImplTest {
 
     @Test
     public void shouldReturnNoteDtoAddedToPerson(){
+        //given
+        given(personDao.findById(1)).willReturn(Optional.ofNullable(person));
+        //when
+        List<NoteDto> listNoteFromPerson = personServiceImpl.getListNoteFromPerson(1);
+        //then
+        assertThat(listNoteFromPerson , hasSize(3));
+    }
 
+
+    // -------------- List<ProjectDto> getPersonProjectList(Integer personId) -------------
+    @Test
+    public void shouldReturnProjectListForPerson() {
+        //given
+        given(personDao.findById(1)).willReturn(Optional.of(person));
+        //when
+        List<ProjectDto> personProjectList = personServiceImpl.getPersonProjectList(1);
+        //then
+        assertThat(personProjectList, hasSize(3));
     }
 
 
@@ -112,5 +135,34 @@ class PersonServiceImplTest {
         person2.setLastName("Test");
 
         return Arrays.asList(person, person1, person2);
+    }
+
+
+    private List<Note> getListNote() {
+        Note note = new Note();
+        note.setId(1);
+        note.setTitle("Zadanie1");
+        Note note1 = new Note();
+        note1.setId(2);
+        note1.setTitle("Zadanie2");
+        Note note2 = new Note();
+        note2.setId(3);
+        note2.setTitle("Zadanie3");
+        return Arrays.asList(note, note1, note2);
+    }
+
+
+
+    private List<Project> getListProject() {
+        Project project1 = new Project();
+        project1.setId(1);
+        project1.setName("Projekt1");
+        Project project2 = new Project();
+        project2.setId(2);
+        project2.setName("Projekt2");
+        Project project3 = new Project();
+        project3.setId(3);
+        project3.setName("Projekt3");
+        return Arrays.asList(project1, project2, project3);
     }
 }
